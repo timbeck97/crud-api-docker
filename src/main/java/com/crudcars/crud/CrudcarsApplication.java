2230545aecbded276d1d78e4f9c5913faa332741
Tim Morgenstern <timbeck1997@hotmail.com>
2023-03-03 20:40:07 -0300

    create project

---
+++ src/main/java/com/crudcars/crud/CrudcarsApplication.java
@@ -0,0 +1,13 @@
+package com.crudcars.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudcarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudcarsApplication.class, args);
+	}
+
+}
